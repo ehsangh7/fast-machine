113182a37542d4b75d7d5ca78dedaf25ddead357
ehsan <ehsanghochagh@gmail.com>
2021-08-22 07:58:32 +0430

    create first body in query api

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import List
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+import spacy
+
+nlp_en = spacy.load("en_core_web_sm")
 
 app = FastAPI()
 
@@ -11,5 +13,8 @@ class Article(BaseModel):
 
 @app.post('/article/')
 def recognize_entities(article:Article, big_model: bool = False):
-    return {"message": f"Reading {article.content}", "big_model": big_model}
-
+    doc_en = nlp_en(article.content)
+    ents = []
+    for ent in doc_en.ents:
+        ents.append({"text": ent.text, "label": ent.label_})
+    return {"message": article.content, "ents": ents, "big_model": big_model}
